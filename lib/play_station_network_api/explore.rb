67a0433636f1fb9413b0ca5cc6c77a653645de29
Vlad <pacMakaveli90@gmail.com>
2021-02-11 09:50:35 +0000

    new(): add new explore endpoint

---
+++ lib/play_station_network_api/explore.rb
@@ -0,0 +1,26 @@
+module PlayStationNetworkAPI
+	class Explore < Client
+		attr_accessor :account_id
+
+		def initialize(account_id = '6462910331343535058')
+			@account_id = account_id
+		end
+
+		# GET https://m.np.playstation.net/api/explore/v1/users/6462910331343535058/hub
+		def hub
+			get([path, account_id, 'hub'].join('/')).parsed_response
+		end
+
+		# GET https://m.np.playstation.net/api/explore/v1/users/6462910331343535058/actionCard
+		def action_card
+			raise 'Access Denied'
+			get([path, account_id, 'actionCard'].join('/')).parsed_response
+		end
+
+	private
+
+		def path
+			'/explore/v1/users'
+		end
+	end
+end
