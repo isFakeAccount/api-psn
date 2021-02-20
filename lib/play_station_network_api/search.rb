36c5b9f67f86b0b5f60e840608613814cc0a1709
Vlad <pacMakaveli90@gmail.com>
2021-02-20 13:54:05 +0000

    feature(): allow searching through multiple buckets using the 'universalSearch' endpoint and add all the available buckets through a new method

--- lib/play_station_network_api/search.rb
+++ lib/play_station_network_api/search.rb
@@ -1,6 +1,33 @@
 module PlayStationNetworkAPI
   class Search < PlayStationNetworkAPI::Client
+    BUCKETS ||= {
+      BLENDED: 'Blended',
+      CONCEPT_GAME: 'ConceptGame',
+      CONCEPT_GAME_ALL: 'ConceptGameAll',
+      CONCEPT_GAME_MOBILE_APP: 'ConceptGameMobileApp',
+      CONCEPT_GAME_PS_NOW: 'ConceptGamePsNow',
+      CONCEPT_GAME_EXCLUDING_PS_NOW: 'ConceptGameExcludingPsNow',
+      CONCEPT_GAME_ADDONS: 'ConceptGameAddons',
+      CONCEPT_APPLICATION_GAME: 'ConceptApplicationGame',
+      CONCEPT_APPLICATION_VIDEO: 'ConceptApplicationVideo',
+      CONCEPT_OTHER_NON_GAME: 'ConceptOtherNonGame',
+      VIDEO_CATALOG: 'VideoCatalog',
+      VIDEO_CATALOG_MOVIE: 'VideoCatalogMovie',
+      VIDEO_CATALOG_TV_SERIES: 'VideoCatalogTVSeries',
+      SOCIAL_ALL_ACCOUNTS: 'SocialAllAccounts',
+      SOCIAL_FRIENDS: 'SocialFriends',
+      SOCIAL_FRIENDS_OF_FRIENDS: 'SocialFriendsOfFriends',
+      SOCIAL_FRIENDS_AND_FRIENDS_OF_FRIENDS: 'SocialFriendsAndFriendsOfFriends',
+      SOCIAL_UNRELATED_AND_FRIENDS_OF_FRIENDS: 'SocialUnrelatedAndFriendsOfFriends',
+      SOCIAL_UNRELATED: 'SocialUnrelated'
+    }.freeze
     
+    # Returns a list of buckets available to pass to .query(domains: [])
+    # 
+    def buckets
+      BUCKETS.keys
+    end
+
     # query [String]
     # limit [Integer] {
     #   min: 1,
@@ -8,24 +35,28 @@ module PlayStationNetworkAPI
     # }
     # country [String]
     # language [String]
-    def query(query, offset: 0, limit: 50)
+    def query(query, offset: 0, limit: 50, buckets: [:CONCEPT_GAME_MOBILE_APP])
+      search_domains = []
+      
+      buckets.each do |bucket|
+        search_domains.push({
+          "domain": BUCKETS[bucket.to_sym],
+          "pagination": {
+            
+            # TODO: It seems to be a Java GraphQL pagination thingy, when present it breaks the offset.
+            # So let's leave it out for now as we don't really need to paginate
+            # "cursor": "10"
+
+            "pageSize": limit,
+            "offset": offset
+          }
+        })
+      end
+
       # https://m.np.playstation.net/api/search/v1/universalSearch
       request = post('/search/v1/universalSearch',
         body: {
-          "domainRequests": [
-            {
-              "domain": "ConceptGameMobileApp",
-              
-              # TODO: It seems to be a Java GraphQL pagination thingy, when present it breaks the offset.
-              # So let's leave it out for now as we don't really need to paginate
-              # "cursor": "10"
-
-              "pagination": {
-                "pageSize": limit,
-                "offset": offset
-              }
-            }
-          ],
+          "domainRequests": search_domains,
           "age": "999",
           "countryCode": country,
           "searchTerm": query,
@@ -33,9 +64,22 @@ module PlayStationNetworkAPI
         }.to_json
       )
 
+      buckets = []
+
+      request.dig('domainResponses').map do |domain_response|
+        bucket = {}
+        bucket[domain_response['domain']] = {
+          total_results: domain_response['totalResultCount'],
+          results: domain_response['results']
+        }
+
+        buckets.push(bucket)
+      end
+
       return {
-        total_results: request.dig('domainResponses', 0, 'totalResultCount'),
-        results: request.dig('domainResponses', 0, 'results')
+        query: request['prefix'],
+        suggestions: request['suggestions'],
+        buckets: buckets,
       }
     end
   end
